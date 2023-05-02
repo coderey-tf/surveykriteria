3e9d0a12188263a5f2f490a4dccd9c8d48738809
coderey-tf <reynaldisatriawanw@gmail.com>
2023-05-02 23:28:38 +0700

    update

---
+++ app/components/Navbar/page.tsx
@@ -0,0 +1,9 @@
+export default function Navbar() {
+  return (
+    <div className="navbar bg-base-400">
+      <a className="btn btn-ghost normal-case text-xl">
+        Welcome To My Survey!!
+      </a>
+    </div>
+  );
+}
